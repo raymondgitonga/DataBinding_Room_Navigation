b28468253c0bbfa359bef273fba1c0649944a5b5
raymond gitonga <raytosh95@gmail.com>
2020-01-30 13:07:23 +0300

    Add navigation

---
+++ app/src/main/java/com/tosh/roomnav/HomeFragment.kt
@@ -0,0 +1,27 @@
+package com.tosh.roomnav
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        buttonAdd.setOnClickListener {
+            val action = HomeFragmentDirections.actionAddNote()
+            Navigation.findNavController(it).navigate(action)
+        }
+
+    }
+}
