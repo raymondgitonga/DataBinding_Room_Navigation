b6cc72f44241c19976a14a2aa6fe4f5670e7bea9
raymond gitonga <raytosh95@gmail.com>
2020-02-03 15:29:53 +0300

    Add databinding

---
+++ app/src/main/java/com/tosh/roomnav/ui/NoteClickListener.kt
@@ -0,0 +1,7 @@
+package com.tosh.roomnav.ui
+
+import android.view.View
+
+interface NoteClickListener {
+    fun onNoteClicked(v: View)
+}
--- app/src/main/java/com/tosh/roomnav/ui/NotesAdapter.kt
+++ app/src/main/java/com/tosh/roomnav/ui/NotesAdapter.kt
@@ -3,9 +3,11 @@ package com.tosh.roomnav.ui
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.tosh.roomnav.R
+import com.tosh.roomnav.databinding.ItemNoteBinding
 import com.tosh.roomnav.room.Note
 import kotlinx.android.synthetic.main.item_note.view.*
 
@@ -13,16 +15,19 @@ class NotesAdapter(val notes: List<Note>): RecyclerView.Adapter<NotesAdapter.Not
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.item_note, parent, false)
+
+        val view = DataBindingUtil.inflate<ItemNoteBinding>(
+            inflater, R.layout.item_note, parent, false
+        )
+
         return NoteViewHolder(view)
     }
 
     override fun getItemCount() = notes.size
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
-        holder.view.titleTv.text = notes[position].title
-        holder.view.bodyTv.text = notes[position].body
-        holder.view.setOnClickListener {
+        holder.view.note = notes[position]
+        holder.view.root.setOnClickListener {
             val action = HomeFragmentDirections.actionAddNote()
             action.note = notes[position]
 
@@ -30,5 +35,5 @@ class NotesAdapter(val notes: List<Note>): RecyclerView.Adapter<NotesAdapter.Not
         }
     }
 
-    class NoteViewHolder(val view: View): RecyclerView.ViewHolder(view)
+    class NoteViewHolder(val view: ItemNoteBinding): RecyclerView.ViewHolder(view.root)
 }
